1e6784b0e3844a692d55cacf847f1ea7d2f9f0c2
brunaporato <brunaporato@gmail.com>
2025-03-05 17:51:08 -0300

    feat: create welcome component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Welcome } from "@/components/Welcome";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 22 }}>Hello world</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   );
 }
