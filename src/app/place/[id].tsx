006840b61e22b70789863b4bb1d06816a4a49942
brunaporato <brunaporato@gmail.com>
2025-03-08 13:12:05 -0300

    feat: uses camera to read qrcode and return a valid coupon

--- src/app/place/[id].tsx
+++ src/app/place/[id].tsx
@@ -3,9 +3,12 @@ import { DetailsCover } from "@/components/PlaceScreen/DetailsCover";
 import { Loading } from "@/components/Loading";
 import { api } from "@/services/api";
 import { Redirect, router, useLocalSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
-import { Alert, View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Alert, Modal, View } from "react-native";
 import { Coupon } from "@/components/PlaceScreen/Coupon";
+import { Button } from "@/components/Button";
+import { IconScan } from "@tabler/icons-react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
 
 interface DataProps extends DetailsProps {
   cover: string;
@@ -14,7 +17,14 @@ interface DataProps extends DetailsProps {
 export default function Place() {
   const [data, setData] = useState<DataProps>();
   const [isLoading, setIsLoading] = useState(true);
+  const [coupon, setCoupon] = useState<string | null>(null);
+  const [isFetchingCoupon, setIsFetchingCoupon] = useState(false);
+  const [isCameraModalVisible, setIsCameraModalVisible] = useState(false);
+
+  const [_, requestPermission] = useCameraPermissions();
   const { id } = useLocalSearchParams();
+
+  const qrLock = useRef(false);
 
   async function fetchPlace() {
     try {
@@ -32,9 +42,57 @@ export default function Place() {
     }
   }
 
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission();
+
+      if (!granted) {
+        return Alert.alert(
+          "Câmera",
+          "Você precisa habilitar o acesso à câmera para ler o QR Code."
+        );
+      }
+
+      qrLock.current = false;
+      setIsCameraModalVisible(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não foi possível abrir a câmera.");
+    }
+  }
+
+  async function getCoupon(id: string) {
+    try {
+      setIsFetchingCoupon(true);
+
+      const { data } = await api.patch(`/coupons/${id}`);
+      Alert.alert("Cupom", `Seu cupom é: ${data.coupon}`);
+
+      setCoupon(data.coupon);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não foi possível obter o cupom.");
+    } finally {
+      setIsFetchingCoupon(false);
+    }
+  }
+
+  function handleUseCoupon(id: string) {
+    setIsCameraModalVisible(false);
+
+    Alert.alert(
+      "Cupom",
+      "Não é possível reutilizar um cupom resgatado. Deseja realmente resgatar o cupom?",
+      [
+        { style: "cancel", text: "Não" },
+        { style: "default", text: "Sim", onPress: () => getCoupon(id) },
+      ]
+    );
+  }
+
   useEffect(() => {
     fetchPlace();
-  }, []);
+  }, [id, coupon]);
 
   if (isLoading) {
     return <Loading />;
@@ -49,7 +107,36 @@ export default function Place() {
       <DetailsCover uri={data.cover} />
 
       <Details data={data} />
-      <Coupon code="123456" />
+      {coupon && <Coupon code={coupon} />}
+
+      <View style={{ padding: 32 }}>
+        <Button onPress={handleOpenCamera}>
+          <Button.Icon icon={IconScan} />
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      <Modal style={{ flex: 1 }} visible={isCameraModalVisible}>
+        <CameraView
+          style={{ flex: 1 }}
+          facing="back"
+          onBarcodeScanned={({ data }) => {
+            if (data && !qrLock.current) {
+              qrLock.current = true;
+              handleUseCoupon(data);
+            }
+          }}
+        />
+
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button
+            onPress={() => setIsCameraModalVisible(false)}
+            isLoading={isFetchingCoupon}
+          >
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
